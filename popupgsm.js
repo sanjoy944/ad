973ee69423d37791b2b4b657b8b7abe4a4cb14a5
sanjoy944 <44388753+sanjoy944@users.noreply.github.com>
2024-06-27 18:18:10 +0600

    popupgsm.js

--- popupgsm.js
+++ popupgsm.js
@@ -4,8 +4,8 @@ document.addEventListener("click", function(event) {
   if (!buttonClicked) {
     buttonClicked = true;
     var websiteTitle = document.title;
-    var websiteUrl = "https://gharriesbyzantinesalmonidae";
-    var fullUrl = websiteUrl + ".com/2Nrg27ffd7d591b1d5bf0bd06bc4be178e89655898300?q=" + websiteTitle.replace(" ", "%20");
+    var websiteUrl = "https://crambidnonutilitybayadeer";
+    var fullUrl = websiteUrl + ".com/3xrJF0634a5513029319feaccd2995b01b05088824fbf?q=" + websiteTitle.replace(" ", "%20");
     window.open(fullUrl, "_blank");
     setTimeout(function() {
       buttonClicked = false;
