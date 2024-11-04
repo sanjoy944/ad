7536ae0a5c30c1d69a6743bddcf754147d2b0d50
sanjoy944 <44388753+sanjoy944@users.noreply.github.com>
2024-11-04 18:53:24 +0600

    popupgsm.js

--- popupgsm.js
+++ popupgsm.js
@@ -4,8 +4,8 @@ document.addEventListener("click", function(event) {
   if (!buttonClicked) {
     buttonClicked = true;
     var websiteTitle = document.title;
-    var websiteUrl = "https://intercludehouselinekeest";
-    var fullUrl = websiteUrl + ".com/QArcPe71aabe2f73f8aee33b266c52a0e86786bfb1a18?q=" + websiteTitle.replace(" ", "%20");
+    var websiteUrl = "https://simulcastedificantatopic";
+    var fullUrl = websiteUrl + ".com/ysrphe71aabe2f73f8aee33b266c52a0e86786bfb1a18?q=" + websiteTitle.replace(" ", "%20");
     window.open(fullUrl, "_blank");
     setTimeout(function() {
       buttonClicked = false;
